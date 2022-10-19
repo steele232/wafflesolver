33d3973e4fe8cd7dcc9e2356e3f6fa6be1410ddd
Jonathan Steele <jonathan.steele232@gmail.com>
2022-10-19 12:57:08 -0700

    Basic counter complete

--- lib/demo_web/live/game_live.ex
+++ lib/demo_web/live/game_live.ex
@@ -5,7 +5,31 @@ defmodule DemoWeb.GameLive do
     DemoWeb.GameView.render("index.html", assigns)
   end
 
-  def mount(_session, socket) do
-    {:ok, assign(socket, state: %Demo.State{})}
+  def mount(_params, _session, socket) do
+    # {:ok, assign(socket, state: %Demo.State{})}
+    {:ok, assign(socket, :num, 0)}
+  end
+
+  def handle_event("inc_iteration", _value, socket) do
+    # {:ok, new_temp} = Thermostat.inc_temperature(socket.assigns.id)
+
+    # TODO update the socket state
+    # TODO re-render !!
+    # Map.update(socket.assigns.state, :iterations, 0, fn a -> a + 1 end)
+    # {:noreply, assign(socket, :iterations, 1 + socket.assigns.iterations)}
+    # {:noreply, assign(socket, :state, 1 + socket.assigns.iterations)}
+    {:noreply, assign(socket, :num, 1 + socket.assigns.num)}
+
+  end
+  def handle_event("dec_iteration", _value, socket) do
+    # {:ok, new_temp} = Thermostat.inc_temperature(socket.assigns.id)
+
+    # TODO update the socket state
+    # TODO re-render !!
+    # Map.update(socket.assigns.state, :iterations, 0, fn a -> a + 1 end)
+    # {:noreply, assign(socket, :iterations, 1 + socket.assigns.iterations)}
+    # {:noreply, assign(socket, :state, 1 + socket.assigns.iterations)}
+    {:noreply, assign(socket, :num, -1 + socket.assigns.num)}
+
   end
 end

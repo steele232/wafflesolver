001aa9be858261bca0258de3eb34d2662359b420
Jonathan Steele <jonathan.steele232@gmail.com>
2022-10-19 13:05:56 -0700

    Cleaned up incrementing code

--- lib/demo/state.ex
+++
@@ -1,6 +0,0 @@
-defmodule Demo.State do
-  alias Demo.State
-
-  defstruct iteration: 0, letters: Enum.map(1..25, fn _ -> nil end)
-
-end
--- lib/demo_web/live/game_live.ex
+++ lib/demo_web/live/game_live.ex
@@ -1,35 +1,21 @@
 defmodule DemoWeb.GameLive do
   use Phoenix.LiveView
 
+  @keys [:iteration, :letters]
+
   def render(assigns) do
     DemoWeb.GameView.render("index.html", assigns)
   end
 
   def mount(_params, _session, socket) do
-    # {:ok, assign(socket, state: %Demo.State{})}
-    {:ok, assign(socket, :num, 0)}
+    {:ok, assign(socket, :iteration, 0)}
   end
 
   def handle_event("inc_iteration", _value, socket) do
-    # {:ok, new_temp} = Thermostat.inc_temperature(socket.assigns.id)
-
-    # TODO update the socket state
-    # TODO re-render !!
-    # Map.update(socket.assigns.state, :iterations, 0, fn a -> a + 1 end)
-    # {:noreply, assign(socket, :iterations, 1 + socket.assigns.iterations)}
-    # {:noreply, assign(socket, :state, 1 + socket.assigns.iterations)}
-    {:noreply, assign(socket, :num, 1 + socket.assigns.num)}
+    {:noreply, assign(socket, :iteration, 1 + socket.assigns.iteration)}
 
   end
   def handle_event("dec_iteration", _value, socket) do
-    # {:ok, new_temp} = Thermostat.inc_temperature(socket.assigns.id)
-
-    # TODO update the socket state
-    # TODO re-render !!
-    # Map.update(socket.assigns.state, :iterations, 0, fn a -> a + 1 end)
-    # {:noreply, assign(socket, :iterations, 1 + socket.assigns.iterations)}
-    # {:noreply, assign(socket, :state, 1 + socket.assigns.iterations)}
-    {:noreply, assign(socket, :num, -1 + socket.assigns.num)}
-
+    {:noreply, assign(socket, :iteration, -1 + socket.assigns.iteration)}
   end
 end
